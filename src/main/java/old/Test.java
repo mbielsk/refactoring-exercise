8d4fe53c7e938f88c4ef784e27af19275532ca15
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-10-11 12:02:37 +0200

    Update

--- src/main/java/old/Test.java
+++ src/main/java/old/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         Customer c = new Customer("Kowalski");
         c.addRental(new Rental(new Movie("some movie", 0), 6));
-        c.addRental(new Rental(new Movie("some movie 2", 1), 15));
+        c.addRental(new Rental(new Movie("some movie 2", 1), 6));
         c.addRental(new Rental(new Movie("some movie 3", 2), 7));
         System.out.println(c.statement());
     }
--- src/test/java/pl/movies/PriceCalculatorTest.java
+++ src/test/java/pl/movies/PriceCalculatorTest.java
@@ -8,12 +8,20 @@ public class PriceCalculatorTest {
 
     private static final int  PERIOD_IN_DAYS = 6;
 
-    private PriceCalculator priceCalculator = new PriceCalculator(2, 1.5, 2);
+    private PriceCalculator priceCalculator;
 
     @Test
     public void shouldCalculateRegularMoviePrice() {
+        priceCalculator = new PriceCalculator(2, 1.5, 2);
         double price = priceCalculator.getValueFor(PERIOD_IN_DAYS);
         assertEquals(8.0, price);
+    }
+
+    @Test
+    public void shouldCalculateNewReleaseMoviePrice() {
+        priceCalculator = new PriceCalculator(0, 3, 0);
+        double price = priceCalculator.getValueFor(PERIOD_IN_DAYS);
+        assertEquals(18.0, price);
     }
 
 }
